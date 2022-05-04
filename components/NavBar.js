5b2d9b343b6bbebe532024d6e39eb35cae974b6d
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2022-05-04 10:49:54 +0530

    env changes

--- components/NavBar.js
+++ components/NavBar.js
@@ -70,7 +70,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link  href="/">
-                <a className="navbar-brand">DEVAT</a>
+                <a className="navbar-brand">ONLINEHATT</a>
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
